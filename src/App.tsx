b6210b69bb8a901670ad36a3210914c1a83fb91e
Mathis <eyzex@hotmail.fr>
2022-11-22 11:25:17 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
+
+const Marker = ({
+  text,
+  lat,
+  lng,
+}: {
+  text: string;
+  lat: number;
+  lng: number;
+}) => <div style={{ fontSize: 24 }}>{text}</div>;
 
 function App() {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  useEffect(() => {
+    if (!!navigator.geolocation) {
+      navigator.geolocation.watchPosition(
+        (position) => {
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        (err) => console.error(err),
+        { enableHighAccuracy: true, timeout: 10000, maximumAge: 10000 }
+      );
+    } else {
+      alert('Le navigateur ne supporte pas cela, besoin de votre position');
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={{ height: '100vh', width: '100%' }}>
+      {lat && lng ? (
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDbLPaCMwDuKbkRfjCcjOHySCkR59gLGsM' }}
+          center={{ lat: lat, lng: lng }}
+          defaultZoom={11}
         >
-          Learn React
-        </a>
-      </header>
+          <Marker lat={lat} lng={lng} text="🏡" />
+        </GoogleMapReact>
+      ) : (
+        <p>Besoin de votre position</p>
+      )}
     </div>
   );
 }
